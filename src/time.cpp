340081576bc6b1fdfe298eac562be758224f302f
unknown <sim@.(none)>
2011-06-06 05:36:56 -0700

    use portable timing

--- src/time.cpp
+++ src/time.cpp
@@ -1,23 +1,31 @@
 
-#ifndef WIN32
-#include <sys/time.h>
-#else 
-#include <time.h>
-#endif
+#include <ctime>
 #include "ivymike/time.h"
 
-
-double ivy_mike::gettime(void )
-{
-#ifdef WIN32
-  time_t tp;
-  struct tm localtm;
-  tp = time(NULL);
-  localtm = *localtime(&tp);
-  return 60.0*localtm.tm_min + localtm.tm_sec;
-#else
-  struct timeval ttime;
-  gettimeofday(&ttime , 0);
-  return ttime.tv_sec + ttime.tv_usec * 0.000001;
-#endif
+double ivy_mike::gettime(void) {
+	return clock() / double(CLOCKS_PER_SEC);
 }
+
+
+//#ifndef WIN32
+//#include <sys/time.h>
+//#else 
+//#include <time.h>
+//#endif
+//#include "ivymike/time.h"
+//
+//
+//double ivy_mike::gettime(void )
+//{
+//#ifdef WIN32
+//  time_t tp;
+//  struct tm localtm;
+//  tp = time(NULL);
+//  localtm = *localtime(&tp);
+//  return 60.0*localtm.tm_min + localtm.tm_sec;
+//#else
+//  struct timeval ttime;
+//  gettimeofday(&ttime , 0);
+//  return ttime.tv_sec + ttime.tv_usec * 0.000001;
+//#endif
+//}

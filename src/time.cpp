ef2312d6b28c001636514a5c8cb9b12379545634
Simon Berger <simerger@gmail.com>
2011-06-06 14:53:10 +0200

    revert last change

--- src/time.cpp
+++ src/time.cpp
@@ -1,31 +1,22 @@
 
+
+
+
+#ifndef WIN32
+#include <sys/time.h>
+#else 
 #include <ctime>
+#endif
 #include "ivymike/time.h"
 
-double ivy_mike::gettime(void) {
-	return clock() / double(CLOCKS_PER_SEC);
+
+double ivy_mike::gettime(void )
+{
+#ifdef WIN32
+ return clock() / double(CLOCKS_PER_SEC);
+#else
+ struct timeval ttime;
+ gettimeofday(&ttime , 0);
+ return ttime.tv_sec + ttime.tv_usec * 0.000001;
+#endif
 }
-
-
-//#ifndef WIN32
-//#include <sys/time.h>
-//#else 
-//#include <time.h>
-//#endif
-//#include "ivymike/time.h"
-//
-//
-//double ivy_mike::gettime(void )
-//{
-//#ifdef WIN32
-//  time_t tp;
-//  struct tm localtm;
-//  tp = time(NULL);
-//  localtm = *localtime(&tp);
-//  return 60.0*localtm.tm_min + localtm.tm_sec;
-//#else
-//  struct timeval ttime;
-//  gettimeofday(&ttime , 0);
-//  return ttime.tv_sec + ttime.tv_usec * 0.000001;
-//#endif
-//}

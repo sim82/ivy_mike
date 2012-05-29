a195d0337c0d729bcf9835b21f96a8dee5f8ca37
Simon Berger <bergers@in.tum.de>
2012-05-29 19:17:51 +0200

    switched to lgpl 3 license.

--- src/ivymike/concurrent.h
+++ src/ivymike/concurrent.h
@@ -1,15 +1,20 @@
 /*
- * Copyright (C) 2011 Simon A. Berger
+ * Copyright (C) 2009-2012 Simon A. Berger
+ * 
+ * This file is part of ivy_mike.
+ * 
+ *  ivy_mike is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- *  This program is free software; you may redistribute it and/or modify its
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
+ *  ivy_mike is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- *  for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with ivy_mike.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __ivy_mike__concurrent_h
